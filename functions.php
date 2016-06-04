95320d9cfa1b5ac55d4a815850c600d8d5c07035
Patrick Luzolo <eldorplus@gmail.com>
2016-06-04 15:20:34 +0200

    Adding new functions
    Adding new functions
     - ISBN13toISBN10,
     - getAmazonCover

--- functions.php
+++ functions.php
@@ -22,13 +22,43 @@ if (! function_exists('cleanIsbn')) {
     }
 }
 
+if(! function_exists('ISBN13toISBN10')){
+    
+    /**
+     * @param $isbn
+     * @return string
+     */
+    function ISBN13toISBN10($isbn) {
+
+        if (strlen($isbn) !== 13)
+            return $isbn;
+
+        if (preg_match('/^\d{3}(\d{9})\d$/', $isbn, $m)) {
+            $sequence = $m[1];
+            $sum = 0;
+            $mul = 10;
+            for ($i = 0; $i < 9; $i++) {
+                $sum = $sum + ($mul * (int) $sequence{$i});
+                $mul--;
+            }
+            $mod = 11 - ($sum%11);
+            if ($mod == 10) {
+                $mod = "X";
+            }
+            else if ($mod == 11) {
+                $mod = 0;
+            }
+            $isbn = $sequence.$mod;
+        }
+        return $isbn;
+    }
+}
+
 if (! function_exists('image_matirial')) {
 
     /**
-     * @param $isbn
-     * @param array $replace
-     * @param string $delimiter
-     * @return mixed|string
+     * @param $material
+     * @return string
      */
     function image_matirial($material) {
 
@@ -51,5 +81,29 @@ if (! function_exists('image_matirial')) {
             $url = url('images/icon_vhs_16x16.png');
         }
         return $url;
+    }
+}
+
+if (! function_exists('getAmazonCover')){
+    
+    /**
+     * @param $isbn
+     * @return string
+     */
+    function getAmazonCover($isbn) {
+
+        $isbn = ISBN13toISBN10(preg_replace('/[^\d]/','',$isbn));
+
+        if($isbn != '') {
+            $cover = 'https://images-eu.ssl-images-amazon.com/images/P/'.$isbn.'.MZZZZZZZ.jpg';
+            list($width, $height, $type, $attr) = getimagesize($cover);
+            if ($width == 1 and $height == 1) {
+                $cover = asset('/images/no_book_cover.jpg');
+            }
+        }
+        else {$cover = asset('/images/no_book_cover.jpg');}
+
+        return $cover;
+
     }
 }
